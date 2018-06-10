04fa155570a90f9e558c8326dedcf199c799487c
JoeyWelvaadt1999 <joeywelvaadt@gmail.com>
2018-06-10 16:48:20 +0200

    Art

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+	[SerializeField]private GameObject _levelSelect;
+	// Use this for initialization
+	void Start () {
+		_levelSelect.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenLevelSelect(bool open) {
+		_levelSelect.SetActive (open);
+	}
+
+	public void SelectLevel(string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+}
